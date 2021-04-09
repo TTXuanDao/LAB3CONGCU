9d09e7111ef77e70a4afc3601bba9b054306857e
TTXuanDao <xdao04091999@gmail.com>
2021-04-09 10:24:51 +0700

    them 1 class

---
+++ Lab3/ProjectMVC/ProjectMVC/Controllers/LabGit3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class LabGit3Controller : Controller
+    {
+        // GET: LabGit3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
