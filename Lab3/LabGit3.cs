59069b9f084e69e9ef5ddd20cd56dd369cb9a696
TTXuanDao <xdao04091999@gmail.com>
2021-04-09 10:42:50 +0700

    them 1 class va push len sever

---
+++ Lab3/LabGit3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class LabGit3Controller : Controller
+    {
+        // GET: LabGit3
+        public ActionResult Index()
+        {
+            return View();
+            //commmit lại bản mới
+            //Them cái mới 
+            //Sửa cái cũ
+        }
+    }
+}
